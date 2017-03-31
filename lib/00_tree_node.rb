b739ad59728524b797d0d535765f47585f9fb7d5
App Academy Student <student@appacademy.io>
2017-03-31 11:01:56 -0700

    Write PolyTreeNode with #add_child, #remove_child, #parent=, and others

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,3 +1,35 @@
 class PolyTreeNode
+  attr_reader :parent, :children
+  attr_accessor :value
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(new_parent)
+    @parent.children.delete(self) unless @parent.nil?
+
+    @parent = new_parent
+    @parent.children << self unless @parent.nil?
+  end
+
+  def add_child(child)
+    child.parent = self
+  end
+
+  def remove_child(child)
+    raise "Not yo' child" unless child?(child)
+    child.parent = nil
+  end
+
+  def child?(adoptee)
+    children.include?(adoptee)
+  end
+
+  def to_s
+    @value.to_s
+  end
 
 end
